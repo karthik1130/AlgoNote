28dd0d3a503520c57735951b51f5b121d0960b50
Karthikeya <bairojukarthikeya@gmail.com>
2024-10-10 20:08:57 +0530

    initial config & setup

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -3,23 +3,20 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Home from "./pages/Home/Home";
 import Login from "./pages/Login/Login";
 import Signup from "./pages/Signup/Signup";
+import "./index.css";
 
 const routes = (
   <Router>
     <Routes>
-      <Route path="/dashboard"  element={<Home />} />
-      <Route path="/login"  element={<Login />} />
-      <Route path="/signup"  element={<Signup />} />
-    </Routes  >
+      <Route path="/home" element={<Home />} />
+      <Route path="/login" element={<Login />} />
+      <Route path="/signup" element={<Signup />} />
+    </Routes>
   </Router>
 );
 
 const App = () => {
-  return (
-    <div>
-      {routes}
-    </div>
-  );
+  return <div>{routes}</div>;
 };
 
 export default App;
--- Frontend/src/components/Navbar/Navbar.jsx
+++ Frontend/src/components/Navbar/Navbar.jsx
@@ -2,7 +2,9 @@ import React from "react";
 
 const Navbar = () => {
   return (
-    <div>Navbar</div>
+    <div className="bg-white flex items-center justify-between px-6 py-2 drop-shadow">
+      <h2 className="text-xl font-medium text-black py-2">AlgoNote</h2>
+    </div>
   );
 };
 
