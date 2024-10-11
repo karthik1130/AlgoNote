b84a75359bc252f79afba690ff2df76643c16a01
Karthikeya <bairojukarthikeya@gmail.com>
2024-10-11 10:52:05 +0530

    navbar profile card added

--- Frontend/src/components/Navbar/Navbar.jsx
+++ Frontend/src/components/Navbar/Navbar.jsx
@@ -1,11 +1,23 @@
 import React from "react";
 import logo from "../../assets/icon.png";
+import Profile from "../Cards/Profile";
+import { useNavigate } from "react-router-dom";
 
 const Navbar = () => {
+  const navigate = useNavigate;
+
+  const onLogout = () => {
+    navigate("/login");
+  };
+
   return (
-    <div className="bg-white flex items-center justify-left px-6 py-2 drop-shadow">
-      <img src={logo} alt="AlgoNote logo" className="h-9 w-9 mr-2" />
-      <h2 className="text-2xl font-medium text-black py-2">AlgoNote</h2>
+    <div className="bg-white flex items-center justify-between px-6 py-2 drop-shadow">
+      <div className="flex items-center">
+        <img src={logo} alt="AlgoNote logo" className="h-9 w-9 mr-2" />
+        <h2 className="text-2xl font-medium text-black py-2">AlgoNote</h2>
+      </div>
+
+      <Profile className="ml-auto" onLogout={onLogout} />
     </div>
   );
 };
--- Frontend/src/utils/helper.js
+++ Frontend/src/utils/helper.js
@@ -2,3 +2,15 @@ export const verifyEmail = (email) => {
   const regex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
   return regex.test(email);
 };
+
+export const getInitials = (name) => {
+  if (!name) return "";
+  const words = name.split(" ");
+  let initinals = "";
+
+  for (let i = 0; i < Math.min(words.length, 2); i++) {
+    initinals += words[i][0];
+  }
+
+  return initinals.toUpperCase();
+};
